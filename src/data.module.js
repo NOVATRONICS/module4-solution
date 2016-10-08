5c6e9666832bf7fe48c8c4a4ec209a70656a2882
Evgeniy <Evgeniy>
2016-10-09 00:28:16 +0800

    ApiBasePath changed to https

--- src/data.module.js
+++ src/data.module.js
@@ -3,7 +3,7 @@
 angular.module('data', []);
 
 angular.module('data')
-.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com")
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
 .config(function () {
   console.log("data config fired.");
 })
